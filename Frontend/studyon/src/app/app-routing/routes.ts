3dc0d9145c97571dfd98f8ba87c5a4d1d4dfa651
Pocoder <44253480+DenisMakarov@users.noreply.github.com>
2020-09-17 23:23:44 +0300

    studyon inner divs

--- Frontend/studyon/src/app/app-routing/routes.ts
+++ Frontend/studyon/src/app/app-routing/routes.ts
@@ -9,9 +9,9 @@ import { StudyonComponent } from '../studyon/studyon.component';
 
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
-  { path: 'studyons', component: StudyonsComponent},
-  { path: 'people', component: PeopleComponent},
-  { path: 'people/:id', component: ProfileComponent },
+  { path: 'studyons', component: StudyonsComponent, canActivate: [AuthGuard]},
+  { path: 'people', component: PeopleComponent, canActivate: [AuthGuard]},
+  { path: 'people/:id', component: ProfileComponent},
   { path: 'studyons/:id', component: StudyonComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
